10b50b5feb11d021693edbc596148c401c5681ab
Sam <samzaldivar98@gmail.com>
2018-09-30 14:55:24 -0400

    added functions

--- Patient Fees/main.cpp
+++ Patient Fees/main.cpp
@@ -11,23 +11,29 @@ using namespace std;
 
 class PatientAccount{
 private:
-    double total;
+    double charge;
     int numOfDays;
 
 public:
-    double dailyRate;
+    double dailyRate = 500.00;
     
     //Methods
     double getTotal();
     int getNumOfDays();
+    
+    void addCharge(double);
 };
 
 double PatientAccount::getTotal(){
-    return this->total;
+    return (this->numOfDays * this->dailyRate) + this->charge;
 }
 
 int PatientAccount::getNumOfDays(){
     return this->numOfDays;
+}
+
+void PatientAccount::addCharge(double c){
+    this->charge += c;
 }
 
 
@@ -147,6 +153,15 @@ int main() {
 
     
     Surgeries *surgeries[5] = {&s1, &s2, &s3, &s4, &s5};
+    
+    Pharmacy p1("Medication 1", 75);
+    Pharmacy p2("Medication 2", 100);
+    Pharmacy p3("Medication 3", 90);
+    Pharmacy p4("Medication 4", 110);
+    Pharmacy p5("Medication 5", 65);
+
+    
+    Pharmacy *medications[5] = {&p1, &p2, &p3, &p4, &p5};
 
     displaySmenu(surgeries);
     getSinput();
