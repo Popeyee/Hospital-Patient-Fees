81d83f519d55b6cd7ca6cb26d2213a61cf9e1320
Sam <samzaldivar98@gmail.com>
2018-09-30 14:16:38 -0400

    Added Patient Account class

--- Patient Fees/main.cpp
+++ Patient Fees/main.cpp
@@ -9,7 +9,28 @@
 #include <iostream>
 using namespace std;
 
+class PatientAccount{
+private:
+    double total;
+    int numOfDays;
+
+public:
+    double dailyRate;
+    
+    //Methods
+    double getTotal();
+    int getNumOfDays();
+};
+
+double PatientAccount::getTotal(){
+    return this->total;
+}
+
+int PatientAccount::getNumOfDays(){
+    return this->numOfDays;
+}
+
 int main() {
-    cout << "enter your selec"
+  
     return 0;
 }
